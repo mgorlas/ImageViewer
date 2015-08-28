74cd3261ee492952c50271a3e44a655b203aa8ac
Marzena <marzena.gorlas@gmail.com>
2015-08-28 14:16:50 +0200

    Created DirectoryChooser for button action

--- src/main/java/com/capgemini/starterkit/javafx/controller/ImageSearchController.java
+++ src/main/java/com/capgemini/starterkit/javafx/controller/ImageSearchController.java
@@ -1,15 +1,29 @@
 package com.capgemini.starterkit.javafx.controller;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
 
+import com.capgemini.starterkit.javafx.model.ImageSearch;
+
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
-import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Controller for the person search screen.
@@ -25,33 +39,65 @@ public class ImageSearchController {
 
 	private static final Logger LOG = Logger.getLogger(ImageSearchController.class);
 
-	/**
-	 * <p>
-	 * NOTE: The variable name must be {@code resources}.
-	 * </p>
-	 */
 	@FXML
 	private ResourceBundle resources;
 
-	/**
-	 * <p>
-	 * NOTE: The variable name must be {@code location}.
-	 * </p>
-	 */
 	@FXML
 	private URL location;
 
-	@FXML Button searchButton;
+	@FXML
+	Button searchButton;
 
-	@FXML TableView tableImage;
+	@FXML
+	TableView<File> tableImage;
 
-	@FXML TableColumn columnImage;
+	@FXML
+	TableColumn<File, String> columnImage;
 
-	private String folderURL;
+	private final ImageSearch model = new ImageSearch();
 
-	@FXML public void searchButtonAction(ActionEvent event) {
+	private File directory;
 
+	@FXML ImageView imageView;
 
+	@FXML
+	private void initialize() {
+		initializeResultTable();
+		columnImage.textProperty().bindBidirectional(model.nameProperty());
+		tableImage.itemsProperty().bind(model.resultProperty());
+	}
+
+	@FXML
+	public void searchButtonAction(ActionEvent event) {
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		this.directory = directoryChooser.showDialog(new Stage());
+		File[] fileImages = directory.listFiles();
+		model.setResult(new ArrayList<File>(Arrays.asList(fileImages)));
+		tableImage.getSortOrder().clear();
+	}
+
+	private void initializeResultTable() {
+		columnImage.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getName()));
+		tableImage.setPlaceholder(new Label(resources.getString("table.emptyText")));
+
+		tableImage.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<File>() {
+
+			@Override
+			public void changed(ObservableValue<? extends File> observable, File oldValue, File newValue) {
+				LOG.debug(newValue + " selected");
+
+				Task<Void> imageTask = new Task<Void>() {
+
+					@Override
+					protected Void call() throws Exception {
+						imageView.setImage(new Image(newValue.getPath()));
+						imageView.autosize();
+						return null;
+					}
+				};
+				new Thread(imageTask).start();
+			}
+		});
 	}
 
 }
--- src/main/java/com/capgemini/starterkit/javafx/model/ImageSearch.java
+++ src/main/java/com/capgemini/starterkit/javafx/model/ImageSearch.java
@@ -1,7 +1,14 @@
 package com.capgemini.starterkit.javafx.model;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
 
 /**
  * Data displayed image on screen.
@@ -11,7 +18,35 @@ import javafx.beans.property.StringProperty;
 public class ImageSearch {
 
 	private final StringProperty name = new SimpleStringProperty();
+	private final ListProperty<File> result = new SimpleListProperty<>(
+			FXCollections.observableList(new ArrayList<>()));
 
 
+	public final String getName() {
+		return name.get();
+	}
 
+	public final void setName(String value) {
+		name.set(value);
+	}
+
+	public StringProperty nameProperty() {
+		return name;
+	}
+
+	public final List<File> getResult() {
+		return result.get();
+	}
+
+	public final void setResult(List<File> value) {
+		result.setAll(value);
+	}
+
+	public ListProperty<File> resultProperty() {
+		return result;
+	}
+	@Override
+	public String toString() {
+		return "Image Search [name=" + name + ", result=" + result + "]";
+	}
 }
